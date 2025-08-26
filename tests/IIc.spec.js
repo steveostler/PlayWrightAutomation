9a938ff9461369ad8421888af0085e9071c0716a
Stephen Ostler <stephenjostler@gmail.com>
2025-08-26 19:32:38 +0100

    Added getbtlabel examples

---
+++ tests/IIc.spec.js
@@ -0,0 +1,16 @@
+import {test, expect} from '@playwright/test';
+
+test('Playwright special locators', async ({page})=>{
+
+    await page.goto("https://rahulshettyacademy.com/angularpractice/");
+    await page.getByLabel("Check me out if you Love IceCreams!").check();
+    await page.getByLabel("Employed").check();
+    await page.getByLabel("Gender").selectOption("Female");
+    await page.getByPlaceholder("Password").fill("abc123");
+    await page.getByRole("button", {name:"Submit"}).click();
+    const visible = await page.getByText("Success! The Form has been submitted successfully!.").isVisible();
+    await page.getByRole("link" , {name:"Shop"}).click();
+    await page.locator("app-card").filter({hasText: 'Nokia Edge'}).getByRole("button").click();
+
+
+});
